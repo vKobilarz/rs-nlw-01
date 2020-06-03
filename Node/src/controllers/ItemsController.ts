4605acb9fbfa689b158e31a5f71e99c116f7ee81
vKobilarz <vkobilarz@gmail.com>
2020-06-02 21:34:38 -0300

    show, create, update, delete operations created for items

--- Node/src/controllers/ItemsController.ts
+++ Node/src/controllers/ItemsController.ts
@@ -15,10 +15,82 @@ class ItemsController {
     return response.json(serializedItems);
   }
 
-  async show(request: Request, response: Response) {}
-  async create(request: Request, response: Response) {}
-  async update(request: Request, response: Response) {}
-  async delete(request: Request, response: Response) {}
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const item = await knex('items').where('id', id).first();
+
+    if (!item) {
+      return response
+        .status(400)
+        .json({ error: `Item with ID ${id} not found` });
+    }
+
+    const serializedItem = {
+      id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+
+    return response.json(serializedItem);
+  }
+
+  async create(request: Request, response: Response) {
+    const { title } = request.body;
+
+    const item = {
+      title,
+      image:
+        'https://images.unsplash.com/photo-1554486855-60050042cd53?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+    };
+
+    const [id] = await knex('items').insert(item);
+
+    return response.json({
+      id,
+      ...item,
+    });
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title } = request.body;
+
+    const trx = await knex.transaction();
+
+    const item = await trx('items').where('id', id).first();
+
+    if (!item) {
+      return response
+        .status(400)
+        .json({ error: `Item with ID ${id} not found` });
+    }
+
+    await trx('items').update({ title }).where('id', id);
+
+    await trx.commit();
+
+    return response.json({ ...item, id, title });
+  }
+
+  async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const trx = await knex.transaction();
+
+    const item = await trx('items').where('id', id).first();
+
+    if (!item) {
+      return response
+        .status(400)
+        .json({ error: `Item with ID ${id} not found` });
+    }
+
+    await trx('items').delete().where('id', id);
+
+    await trx.commit();
+
+    return response.json({ ...item, id });
+  }
 }
 
 export default new ItemsController();
--- Node/src/routes.ts
+++ Node/src/routes.ts
@@ -6,10 +6,16 @@ import itemsController from './controllers/ItemsController';
 const routes = express.Router();
 
 routes.get('/items', itemsController.index);
+routes.get('/items/:id', itemsController.show);
+routes.post('/items', itemsController.create);
+routes.put('/items/:id', itemsController.update);
+routes.delete('/items/:id', itemsController.delete);
 
-routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+// routes.post('/points', pointsController.create);
+// routes.put('/points/:id', pointsController.update);
+// routes.delete('/points/:id', pointsController.delete);
 
 export default routes;
 
